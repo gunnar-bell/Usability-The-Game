a06695800d2814b5a11e8d837a2a18eb3beb429a
Gunrar Barbell <justGunnar@Gunnars-MacBook-Air-2.local>
2015-07-18 03:10:31 -0400

    initial battle stuff

--- app/scripts/models/player-model.js
+++ app/scripts/models/player-model.js
@@ -3,11 +3,20 @@ define([
     ], function(SpaceshipModel) {
   var PlayerModel = function() {
     this.init = function() {
+      console.log('building playerModel!!');
       this.spaceship = new SpaceshipModel();
-      this.spaceship.refuel(); // for testing
+      this.health = 90;
       this.resources = {'stardust':5};
       this.currentPlanet = 'earth';
-      console.log('building playerModel!!');
+      this.strength = 10;
+      this.attack = function(attackee) {
+        if (Math.random() > .2) { // 80% chance to hit always?
+          attackee.health -= this.strength;
+          console.log(attackee + 'hit for ' + this.strength)
+        } else {
+          console.log('missed!');
+        }
+      }
     };
     this.position  = {'x':100,'y':100};//this.currentPlanet.location;
   };
