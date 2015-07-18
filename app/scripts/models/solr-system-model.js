0d6660d25c620e54e0d3ac9c7e25ac84697f38b7
Gunnar Bell <justGunnar@Gunnars-MacBook-Air-2.local>
2015-07-17 23:53:03 -0400

    forgot system model

---
+++ app/scripts/models/solr-system-model.js
@@ -0,0 +1,8 @@
+define(['planetModel'], function() {
+	var SolrSystemModel = function(options) {
+		this.planets = ['1', '2', '3'];
+		this.moralStanding = 'GOOD|EVIL';
+	}
+
+	return SolrSystemModel;
+});
