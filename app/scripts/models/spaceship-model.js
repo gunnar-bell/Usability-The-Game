541672577bf9f7438866043fb59b1ee67ba41a63
Jess Rucinski <jrucinski@wayfair.com>
2015-07-17 22:25:56 -0400

    refuel

---
+++ app/scripts/models/spaceship-model.js
@@ -0,0 +1,17 @@
+define([], function() {
+  var SpaceshipModel = function(options) {
+    this.init = function() {
+      this.fuel = '1'; // between 0 and 1
+    };
+
+    this.refuel = function() {
+      while (this.fuel < 1) {
+        setInterval(function(){
+          this.fuel += 0.05;
+        }, 500);
+      }
+    };
+  };
+
+  return SpaceshipModel;
+});
